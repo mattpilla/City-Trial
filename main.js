c876e8b64674217593cacbf3fd6f294b085bd072
mattpilla <kaztalek@gmail.com>
2019-09-07 17:27:55 -0400

    start of checklist

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-var ct = new Vue({
-    el: '#ct',
+var app = new Vue({
+    el: '#app',
     data: {
-        //
+        sections: [
+            {
+                title: 'Busts',
+                show: true,
+                checks: [
+                    'Compact > Scooter',
+                    'Warp > Bike',
+                    'Bike > Swerve',
+                    'Swerve > Warp',
+                    'Slick > Rocket',
+                    'Rocket > Turbo',
+                    'Turbo > Formula',
+                    'Formula > Slick'
+                ]
+            }
+        ]
     },
     mounted() {
        //
