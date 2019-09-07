393265a8123a60723c5bc64f3648eda1c9bc8f2f
mattpilla <kaztalek@gmail.com>
2019-09-07 17:54:42 -0400

    more checks

--- main.js
+++ main.js
@@ -6,25 +6,39 @@ var app = new Vue({
                 title: 'Busts',
                 show: true,
                 checks: [
-                    'Compact > Scooter',
-                    'Warp > Bike',
-                    'Bike > Swerve',
-                    'Swerve > Warp',
-                    'Slick > Rocket',
-                    'Rocket > Turbo',
-                    'Turbo > Formula',
-                    'Formula > Slick'
+                    {name: 'Compact > Scooter', checked: false},
+                    {name: 'Warp > Bike', checked: false},
+                    {name: 'Bike > Swerve', checked: false},
+                    {name: 'Swerve > Warp', checked: false},
+                    {name: 'Slick > Rocket', checked: false},
+                    {name: 'Rocket > Turbo', checked: false},
+                    {name: 'Turbo > Formula', checked: false},
+                    {name: 'Formula > Slick', checked: false}
+                ]
+            },
+            {
+                title: 'Food',
+                show: true,
+                checks: [
+                    {name: '3 Sushi', checked: false},
+                    {name: '3 Hot Dog', checked: false},
+                    {name: '3 Energy Drink', checked: false},
+                    {name: '2 Tomato', checked: false}
+                ]
+            },
+            {
+                title: 'Patches',
+                show: true,
+                checks: [
+                    {name: '10 Boost', checked: false},
+                    {name: '10 Top Speed', checked: false},
+                    {name: '10 Turn', checked: false},
+                    {name: '10 Charge', checked: false},
+                    {name: '10 Glide', checked: false},
+                    {name: '10 Weight', checked: false},
+                    {name: '10 Defense', checked: false}
                 ]
             }
         ]
-    },
-    mounted() {
-       //
-    },
-    watch: {
-        //
-    },
-    methods: {
-        //
     }
 });
